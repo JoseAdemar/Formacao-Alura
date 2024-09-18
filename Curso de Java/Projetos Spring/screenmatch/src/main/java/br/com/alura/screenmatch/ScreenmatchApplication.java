d54062d67cfe10a7e8b5e2371f97d86fef9600ab
joseademar <joseademarti@gmail.com>
2024-09-18 18:36:58 -0300

    Organizado o projeto e feito a implementação de classe de dados e service

--- Curso de Java/Projetos Spring/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Curso de Java/Projetos Spring/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,9 +16,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		String url = "http://www.omdbapi.com/?t=Game%20of%20Thrones&Season=1&apikey=d4ee72a2";
+		String url = "http://www.omdbapi.com/?t=Game%20of%20Thrones&apikey=d4ee72a2";
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados(url);
 		System.out.println(json);
+		System.out.println("--------------------------------------------");
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
