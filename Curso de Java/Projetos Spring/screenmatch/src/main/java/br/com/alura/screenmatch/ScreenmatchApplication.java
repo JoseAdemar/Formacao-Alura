c525bc5e0b4a48c78c8d57f73b1adef258840d86
joseademar <joseademarti@gmail.com>
2024-09-19 17:57:00 -0300

    Modelado a classe de dados de temporadas.

--- Curso de Java/Projetos Spring/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ Curso de Java/Projetos Spring/screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -19,10 +24,29 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		String url = "http://www.omdbapi.com/?t=Game%20of%20Thrones&apikey=d4ee72a2";
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados(url);
-		System.out.println(json);
-		System.out.println("--------------------------------------------");
+		//System.out.println(json);
+		//System.out.println("--------------------------------------------");
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		System.out.println("----------------------------------------");
+
+		url = "https://www.omdbapi.com/?t=Game%20of%20Thrones&Season=1&episode=2&apikey=d4ee72a2";
+		json = consumoApi.obterDados(url);
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+		System.out.println("----------------------------------------");
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+		DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+		for (int i = 1;  i <= dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=Game%20of%20Thrones&Season="+ i + "&apikey=d4ee72a2");
+			dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+		}
+
+		temporadas.add(dadosTemporada);
+		//temporadas.forEach(System.out::println);
+		temporadas.stream().sorted()
+				.forEach(System.out::println);
 	}
 }
