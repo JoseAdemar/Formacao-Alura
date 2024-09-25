cf8a3f713f680ba5a803a0964346a6bd11342cbb
joseademar <joseademarti@gmail.com>
2024-09-25 18:28:20 -0300

    Trabalhando no desafio

---
+++ Curso de Java/Projetos Spring/TabelaFipe/src/main/java/com/alura/TabelaFipe/model/Dados.java
@@ -0,0 +1,5 @@
+package com.alura.TabelaFipe.model;
+
+public record Dados(String codigo, String nome) {
+
+}
--- Curso de Java/Projetos Spring/TabelaFipe/src/main/java/com/alura/TabelaFipe/principal/Principal.java
+++ Curso de Java/Projetos Spring/TabelaFipe/src/main/java/com/alura/TabelaFipe/principal/Principal.java
@@ -1,21 +1,27 @@
 package com.alura.TabelaFipe.principal;
 
+import com.alura.TabelaFipe.model.Dados;
+import com.alura.TabelaFipe.model.Modelos;
 import com.alura.TabelaFipe.service.ConsumoApi;
+import com.alura.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
+
+    private ConverteDados conversor = new ConverteDados();
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
-    public void exibirMenu(){
+    public void exibirMenu() {
         var menu = """
                 *** OPÇÕES ***
                 Carro
                 Moto
                 Caminhão
-                
+                                
                 Digite uma das opções para consultar:
                 """;
 
@@ -23,7 +29,7 @@ public class Principal {
         var opcao = leitura.nextLine();
         String endereco;
 
-        if (opcao.toLowerCase().contains("car")){
+        if (opcao.toLowerCase().contains("car")) {
             endereco = URL_BASE + "carros/marcas";
         } else if (opcao.toLowerCase().contains("mot")) {
             endereco = URL_BASE + "motos/marcas";
@@ -32,6 +38,20 @@ public class Principal {
         }
 
         var json = consumo.obterDados(endereco);
-        System.out.println(json);
+        //System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream().sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca para consulta:");
+        var codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumo.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream().sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
     }
 }
