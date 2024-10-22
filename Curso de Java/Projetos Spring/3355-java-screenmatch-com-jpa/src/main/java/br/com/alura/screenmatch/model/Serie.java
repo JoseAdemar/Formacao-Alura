7085ec19ef83cf2e2eb0a0127fed21a9514e2b84
joseademar <joseademarti@gmail.com>
2024-10-22 17:40:49 -0300

    Evoluindo a aplicação para integrar com o frontend

--- Curso de Java/Projetos Spring/3355-java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ Curso de Java/Projetos Spring/3355-java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.model;
 
 import br.com.alura.screenmatch.service.traducao.ConsultaMyMemory;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
