867db0914d007330dcbe94f15c79f64593bd180d
joseademar <joseademarti@gmail.com>
2024-10-31 18:27:08 -0300

    Trabalhando com listas no javascript

--- Curso de front-end/aluramidi-curso-arquivos-iniciais/main.js
+++ Curso de front-end/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,15 @@
 function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+    const listaDeSom = document.querySelectorAll('.som');
+    for (let i = 0; i< listaDeSom.length; i++){
+        listaDeSom[i].play();
+    }
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('tecla');
+
+for (let i = 0; i< listaDeTeclas.length; i++){
+    listaDeTeclas[i].onclick = tocaSomPom[0];
+}
 
