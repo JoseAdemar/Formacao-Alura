e8497957a479d2a93bc98ea67af5bfe0ada330ed
joseademar <joseademarti@gmail.com>
2024-11-07 17:14:57 -0300

    Aprofundando no javascript

--- Curso de front-end/aluramidi-curso-arquivos-iniciais/main.js
+++ Curso de front-end/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,21 +1,40 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(selectorAudio) {
+    const elemento = document.querySelector(selectorAudio);
+    if (elemento === null) {
+
+    } if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        alert('Elemento não encontrado ou seletor inválido')
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
+//let contador = 0;
 
 // Forma 01
-/*for (let i = 0; i < listaDeTeclas.length; i++){
+for (let i = 0; i < listaDeTeclas.length; i++) {
     const tecla = listaDeTeclas[i];
     const sonDoInstrumento = tecla.classList[1];
     const idAudio = `#som_${sonDoInstrumento}`;
-    tecla.onclick = function() {
+    tecla.onclick = function () {
         tocaSom(idAudio);
-    };
-}*/
+    }
+
+    tecla.onkeydown = function (evento) {
+        console.log(evento)
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
 
 //Forma 02
+/*
 while(contador < listaDeTeclas.length) {
     const tecla = listaDeTeclas[contador];
     const sonDoInstrumento = tecla.classList[1];
@@ -26,4 +45,4 @@ while(contador < listaDeTeclas.length) {
 
     contador++;
 }
-
+*/
