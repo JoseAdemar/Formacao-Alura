c981e78e227d4e018c1ffff3849b8db4b00850d6
joseademar <joseademarti@gmail.com>
2025-04-28 14:52:35 -0300

    Estudando maven

--- Curso de Java/Curso de Maven/produtos/src/main/java/br/com/alura/Main.java
+++ Curso de Java/Curso de Maven/produtos/src/main/java/br/com/alura/Main.java
@@ -1,7 +1,26 @@
 package br.com.alura;
 
+import br.com.alura.model.Produto;
+import br.com.alura.service.TraduzProdutoService;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        List<Produto> produtos = new CsvToBeanBuilder(new FileReader("src/main/resources/products.csv"))
+                .withType(Produto.class).build().parse();
+
+        produtos.forEach(System.out::println);
+        System.out.println();
+        TraduzProdutoService traducaoService = new TraduzProdutoService();
+
+        for (Produto produto : produtos) {
+            traducaoService.traduzir(produto);
+            System.out.println(produto);
+        }
+
     }
 }
