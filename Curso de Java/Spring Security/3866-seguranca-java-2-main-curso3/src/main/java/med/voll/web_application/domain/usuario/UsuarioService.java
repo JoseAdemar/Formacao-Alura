27b0fb08ac73486ce60febdf102b6f0ad2baee3b
joseademar <joseademarti@gmail.com>
2025-07-09 21:10:22 -0300

    Avançando com spring security

--- Curso de Java/Spring Security/3866-seguranca-java-2-main-curso3/src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
+++ Curso de Java/Spring Security/3866-seguranca-java-2-main-curso3/src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
@@ -3,6 +3,7 @@ package med.voll.web_application.domain.usuario;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,13 +11,26 @@ public class UsuarioService implements UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
 
-    public UsuarioService(UsuarioRepository usuarioRepository) {
+    private final PasswordEncoder encriptador;
+
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder encriptador) {
         this.usuarioRepository = usuarioRepository;
+        this.encriptador = encriptador;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return usuarioRepository.findByEmailIgnoreCase(username)
                 .orElseThrow(() -> new UsernameNotFoundException("O usuário não foi encontrado!"));
+    }
+
+    public Long salvarusuario(String nome, String email, String senha, Perfil perfil) {
+        String senhaCriptografada = encriptador.encode(senha);
+        Usuario usuario = usuarioRepository.save(new Usuario(nome, email, senhaCriptografada, perfil));
+        return usuario.getId();
+    }
+
+    public void excluir(Long id) {
+        usuarioRepository.deleteById(id);
     }
 }
